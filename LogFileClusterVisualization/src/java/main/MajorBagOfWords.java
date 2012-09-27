1964e3957c1fe37058e8e65643afff169efc3c84
Lekhnath Khanal <ln2khanal@gmail.com>
2012-09-28 00:23:41 +0545

    commented part removed.

--- LogFileClusterVisualization/src/java/main/MajorBagOfWords.java
+++ LogFileClusterVisualization/src/java/main/MajorBagOfWords.java
@@ -23,37 +23,6 @@ public class MajorBagOfWords {
             returnFlag = true;
         }
         return returnFlag;
-//        String dir = "D:/Major/clusters/";
-//
-//        
-//        String message = "1100010100000111011010101010001110";
-//
-//        BitFixer bitsfixer = new BitFixer(message);
-//        bitsfixer.fixBits();
-//        message = bitsfixer.getVal();
-//
-//        PointsFixer pointfixer = new PointsFixer(message);
-//        pointfixer.fixpoints();
-//
-//        CurveFitter curvefitter = new CurveFitter(pointfixer.getVal());
-//        curvefitter.fitTheCurve();
-//        String[] slopeintercept = curvefitter.getVal().split(":");
-//
-//        ParameterComputer parametercomputer = new ParameterComputer(curvefitter.getVal(), message);
-//        parametercomputer.computeParameters();
-//        String[] messageCartesianCoordinates = parametercomputer.getVal().split(",");
-//
-//        String messageInfo = String.format(messageCartesianCoordinates[4] + "_" + messageCartesianCoordinates[5] + "_" + messageCartesianCoordinates[6] + "_" + slopeintercept[0]);
-//        fileoperation.FileChooserWriter filewriter = new FileChooserWriter(messageInfo, message, dir);
-//        filewriter.chooseCluster();
-//
-//
-//        System.out.println("Message: (x1,y1) = (" + messageCartesianCoordinates[0] + "," + messageCartesianCoordinates[1] + ")" + " & (x2,y2) = (" + messageCartesianCoordinates[2] + "," + messageCartesianCoordinates[3] + ")");
-//        System.out.println("Length of thus Message line: " + messageCartesianCoordinates[4]);
-//        System.out.println("Mid position of the line formed[Message]: " + messageCartesianCoordinates[6]);
-//        System.out.println("Standard deviation of the points in the string of[Message]: " + Float.parseFloat(messageCartesianCoordinates[5]));
-        
-
     }
    
 }
