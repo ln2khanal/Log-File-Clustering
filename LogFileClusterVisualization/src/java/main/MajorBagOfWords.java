f7a63202a24a0f400276dae06b3e803d40c94918
Lekhnath Khanal <lekhnath.khanal@logpoint.com>
2014-09-02 20:13:35 +0545

    temp first

--- LogFileClusterVisualization/src/java/main/MajorBagOfWords.java
+++ LogFileClusterVisualization/src/java/main/MajorBagOfWords.java
@@ -2,6 +2,9 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+/*
+*This should be the first commit
+*/
 package main;
 
 import java.io.IOException;
